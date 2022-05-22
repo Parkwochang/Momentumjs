891c9bd7aa0c222b58aa80fbabd1cb4230b4ec37
Parkwochang <whwh3560@gmail.com>
2022-05-22 22:47:58 +0900

    add css

--- js/background.js
+++ js/background.js
@@ -1,7 +1,5 @@
-const images = ["01.jpg", "02.jpg", "03.jpg"];
+const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-
 document.body.style.backgroundImage = `url(img/${chosenImage})`;
-
--- js/greetings.js
+++ js/greetings.js
@@ -1,23 +1,27 @@
-const loginForm = document.getElementById("login_form")
+const loginForm = document.getElementById("login_form");
 const loginIput = document.querySelector("#login_form input");
 const greeting = document.querySelector("#greeting");
 const logoutBtn = document.querySelector("#logout_btn");
+const todo = document.getElementById("todo_form");
 
-const HIDDEN_CLASSNAME = "hidden"
+const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 //submit and remember username
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  const username = loginIput.value;
+  const username = loginIput.value.toUpperCase();
   localStorage.setItem(USERNAME_KEY, username);
   paintGreetings(username);
   showLogoutBtn();
+  todo.classList.remove(HIDDEN_CLASSNAME);
 }
 
 function paintGreetings(username) {
-  greeting.innerText = `Hello ${username}`;
+  greeting.innerText = 
+  `Hello 
+  ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
